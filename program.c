7029beea567f0c1f6243a1f8e9e5d63a77c1e8a4
Kevin-Zhouu <bigsoupzhou@gmail.com>
2022-10-08 12:45:28 +1100

    run

--- program.c
+++ program.c
@@ -625,7 +625,7 @@ candidate_list_t *find_potential_seq(sup_matrix_t *sup_matrix)
             }
         }
     }
-    qsort(can_list->cans, can_index, sizeof(can_list->cans), cmp_cans);
+    qsort(can_list->cans, can_index, sizeof(candidate_t *), cmp_cans);
     printf("------------\n");
     for (int i = 0; i < can_index; i++)
     {
@@ -634,8 +634,8 @@ candidate_list_t *find_potential_seq(sup_matrix_t *sup_matrix)
 }
 int cmp_cans(const void *a, const void *b)
 {
-    candidate_t *can_a = (candidate_t *)a;
-    candidate_t *can_b = (candidate_t *)b;
+    candidate_t *can_a = (candidate_t **)a;
+    candidate_t *can_b = (candidate_t **)b;
     return can_b->w - can_a->w;
 }
 int calc_pd(sup_t *xy, sup_t *yx)
