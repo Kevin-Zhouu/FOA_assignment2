b415f7bb1bc6c5a5d158bd8336d3e8fc408a9aac
Kevin-Zhouu <bigsoupzhou@gmail.com>
2022-10-08 12:36:02 +1100

    run

--- program.c
+++ program.c
@@ -588,7 +588,7 @@ candidate_list_t *find_potential_seq(sup_matrix_t *sup_matrix)
     candidate_list_t *can_list = (candidate_list_t *)malloc(
         sizeof(candidate_list_t));
     can_list->cans = (candidate_t **)malloc(
-        sizeof(candidate_t *));
+        sizeof(cans));
     int can_index = 0;
     int n_rows = sup_matrix->n_rows;
     // looping over the rows
@@ -610,7 +610,7 @@ candidate_list_t *find_potential_seq(sup_matrix_t *sup_matrix)
             {
 
                 candidate_t *can = (candidate_t *)malloc(sizeof(can));
-                can_list->cans = (candidate_list_t **)realloc(
+                can_list->cans = (candidate_t **)realloc(
                     can_list->cans, sizeof(candidate_t *) * (can_index + 1));
                 can->sup = xy;
                 can->pd = pd;
