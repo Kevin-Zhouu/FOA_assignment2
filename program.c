89ec4021f3932ed0732965c4077ee618dad5bf8d
Kevin-Zhouu <bigsoupzhou@gmail.com>
2022-10-08 23:28:42 +1100

    run

--- program.c
+++ program.c
@@ -493,7 +493,6 @@ int add_event_freq(event_freq_t *event_freq_list, int tot_events,
 void calc_stg_1(trace_stats_t *stats)
 {
     sup_matrix_t *sup_matrix = generate_seq_matrix(stats->trace_list, stats);
-    print_matrix(sup_matrix, 1);
     candidate_list_t *can_list = find_potential_seq(sup_matrix);
     int i = 0;
     while (can_list->num != 0 && can_list->cans[0]->sup->x < 256 &&
@@ -501,10 +500,11 @@ void calc_stg_1(trace_stats_t *stats)
     {
 
         stg1_stats_t stg1_stats = del_seq(stats, can_list, (action_t)256 + i);
+
+        print_matrix(sup_matrix, DURING_STAGE);
         print_stg2(&stg1_stats);
         calc_evt_stats(stats);
         sup_matrix = generate_seq_matrix(stats->trace_list, stats);
-        print_matrix(sup_matrix, DURING_STAGE);
         // print_all_trace(stats->trace_list);
         can_list = find_potential_seq(sup_matrix);
         i++;
