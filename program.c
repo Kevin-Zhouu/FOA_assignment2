49593cf6f36400db6fbb0545c353486ab6750c0b
Kevin-Zhouu <bigsoupzhou@gmail.com>
2022-10-12 23:58:00 +1100

    run

--- program.c
+++ program.c
@@ -721,7 +721,7 @@ candidate_t *add_candidate(candidate_list_t *can_list, int *can_index,
     printf("sup(%c,%c) pd=%d w=%d\n", can->sup->x, can->sup->y, pd, w);
     can_list->cans[*can_index] = can;
     *can_index = *can_index + 1;
-    return &can;
+    return can;
 }
 void print_seq(sup_t *sup)
 {
